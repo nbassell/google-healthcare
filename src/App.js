e2870aee7050f967123ab1b5a12b4c3ecd7a413a
agronv <avelaj96@gmail.com>
2019-04-26 11:19:48 -0400

    Start redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './forms.jsx'
 
 function App() {
   return (
---
+++ src/form.js
@@ -0,0 +1,9 @@
+
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { fName: "", Lname}
+  }
+}
